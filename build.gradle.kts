30061f935fa02fba514d239ccc72a9bb1bcecb31
Dan Brough <dan@danbrough.org>
2024-07-12 15:29:48 +1200

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,38 @@
 plugins {
-	alias(libs.plugins.xtras) apply false
-	alias(libs.plugins.kotlin.multiplatform) apply false
-	alias(libs.plugins.kotlin.android) apply false
-	
-	alias(libs.plugins.org.jetbrains.dokka ) apply false
-  alias(libs.plugins.android.application) apply false
-	alias(libs.plugins.android.library) apply false
+  alias(libs.plugins.xtras) apply false
+  alias(libs.plugins.kotlin.multiplatform) apply false
+  alias(libs.plugins.kotlin.android) apply false
 
-	alias(libs.plugins.jetbrains.compose) apply false
-	alias(libs.plugins.compose.compiler) apply false
+  alias(libs.plugins.org.jetbrains.dokka) apply false
+  alias(libs.plugins.android.application) apply false
+  alias(libs.plugins.android.library) apply false
+
+  alias(libs.plugins.jetbrains.compose) apply false
+  alias(libs.plugins.compose.compiler) apply false
+  `maven-publish`
 }
 
 val projectGroup = project.property("project.group").toString()
 val projectVersion = project.property("project.version").toString()
 
-allprojects{
-	group = projectGroup
-	version = projectVersion
+allprojects {
+  group = projectGroup
+  version = projectVersion
+}
+
+subprojects {
+  pluginManager.apply("maven-publish")
+  extensions.findByType<PublishingExtension>()?.apply {
+    repositories {
+      maven("https://maven.pkg.github.com/danbrough/klog") {
+        name = "GitHubPackages"
+
+        credentials {
+          username = System.getenv("USERNAME")
+          password = System.getenv("TOKEN")
+        }
+      }
+    }
+  }
 }
 
