c5d23a5025c623f461989959a099506c10fc5fda
Dan Brough <dan@danbrough.org>
2024-07-14 10:44:28 +1200

    update

--- testlib/build.gradle.kts
+++ testlib/build.gradle.kts
@@ -1,33 +1,4 @@
-import org.danbrough.xtras.androidLibDir
-import org.danbrough.xtras.capitalized
-import org.danbrough.xtras.decapitalized
-import org.danbrough.xtras.envLibraryPathName
-import org.danbrough.xtras.konanDir
-import org.danbrough.xtras.kotlinTargetName
-import org.danbrough.xtras.logDebug
-import org.danbrough.xtras.logError
-import org.danbrough.xtras.logInfo
-import org.danbrough.xtras.pathOf
-import org.danbrough.xtras.supportsJNI
-import org.danbrough.xtras.xtras
-import org.danbrough.xtras.xtrasAndroidConfig
-import org.danbrough.xtras.xtrasDeclareXtrasRepository
 import org.danbrough.xtras.xtrasTesting
-import org.gradle.jvm.tasks.Jar
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
-import org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType
-import org.jetbrains.kotlin.gradle.plugin.mpp.SharedLibrary
-import org.jetbrains.kotlin.gradle.targets.jvm.KotlinJvmTestRun
-import org.jetbrains.kotlin.gradle.targets.jvm.tasks.KotlinJvmTest
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompileCommon
-import org.jetbrains.kotlin.gradle.tasks.KotlinNativeLink
-import org.jetbrains.kotlin.konan.target.Family
-import org.jetbrains.kotlin.konan.target.HostManager
-import org.jetbrains.kotlin.konan.target.KonanTarget
-import org.jetbrains.kotlin.konan.target.presetName
 
 plugins {
   alias(libs.plugins.kotlin.multiplatform)
@@ -35,7 +6,7 @@ plugins {
 
   alias(libs.plugins.xtras)
   //alias(libs.plugins.android.library)
-
+  signing
   alias(libs.plugins.org.jetbrains.dokka)
 }
 
@@ -55,4 +26,10 @@ kotlin {
   }
 }
 
-xtrasTesting {  }
+xtrasTesting { }
+
+signing{
+  publishing.publications.all {
+    sign(this)
+  }
+}
